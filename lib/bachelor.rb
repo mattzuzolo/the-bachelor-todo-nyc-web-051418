21dc77b8d1dd3405abcdbdc8d4c076f50d71d8da
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-01 17:44:55 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,24 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+  
+
+  data.each do |season_key, details|
+    
+    if season_key == season
+    
+        if data["status"] == "Winner"
+          return data["name"].split(" ").first
+        end #end if winner  
+    
+    end #end season if
+
+  end #end data each
+  
+  #return the first name of specified season's winner
+end #end method
+
+#get_first_name_of_season_winner(data, "season 29")
 
 def get_contestant_name(data, occupation)
   # code here
