b117ac3f3b1874f49e768401dafd1323d470f450
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-01 18:18:41 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -72,7 +72,7 @@ def get_average_age_for_season(data, season)
       details.each do |individual_info|
         
         
-       sum_of_age += individual_info["age"].to_i
+       sum_of_age += individual_info["age"].to_f
        counter += 1
         
  
